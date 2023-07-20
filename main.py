e926f01d1ae01656f5b634c544156c279e47f942
catalystfm-zhaocong <115205604+catalystfm-zhaocong@users.noreply.github.com>
2023-07-20 18:46:37 +0800

    Implement gui

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from zc_flightaware_router.gui_window import FlightAwareRouter
+import sys
+from PySide6.QtWidgets import QApplication
 
 if __name__ == "__main__":
-    app = FlightAwareRouter()
-    app.show()
-    app.exec_()
+    app = QApplication(sys.argv)
+    router = FlightAwareRouter()
+    router.show()
+    sys.exit(app.exec())
