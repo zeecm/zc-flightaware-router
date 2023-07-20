5a19ce41bc74715f49a9ecf8e6ab046d25e62155
catalystfm-zhaocong <zhaocong.mah@gmail.com>
2023-07-20 19:08:01 +0800

    Fix type checking and linting

--- zc_flightaware_router/gui_window.py
+++ zc_flightaware_router/gui_window.py
@@ -3,5 +3,5 @@ from zc_flightaware_router.mainwindow import Ui_MainWindow
 
 class FlightAwareRouter(Ui_MainWindow):
     def __init__(self):
-        super(FlightAwareRouter, self).__init__()
+        super().__init__()
         self.setupUi(self)
